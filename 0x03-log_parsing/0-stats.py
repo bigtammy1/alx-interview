e596e15a29f379e672a0b80e13485439d4a4bb2c
Tammy Erekosima <bigtammy1@gmail.com>
2023-07-20 23:07:19 +0000

    Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
+"""Performs log parsing from stdin"""
 
+import re
 import sys
+counter = 0
+file_size = 0
+statusC_counter = {200: 0, 301: 0, 400: 0,
+                   401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-# Initialize variables to store metrics
-total_file_size = 0
-status_code_counts = {}
 
-try:
-    # Define a function to print metrics
-    def print_metrics():
-        print("Total file size: File size:", total_file_size)
-        for status_code in sorted(status_code_counts.keys()):
-            print(f"{status_code}: {status_code_counts[status_code]}")
+def printCodes(dict, file_s):
+    """Prints the status code and the number of times they appear"""
+    print("File size: {}".format(file_s))
+    for key in sorted(dict.keys()):
+        if statusC_counter[key] != 0:
+            print("{}: {}".format(key, dict[key]))
 
-    # Read stdin line by line and compute metrics
-    for line_number, line in enumerate(sys.stdin, start=1):
-        # Remove leading/trailing whitespaces and split the line into parts
-        parts = line.strip().split()
 
-        # Check if the line format is valid
-        if len(parts) != 7 or parts[2] != 'GET' or parts[4] not in status_code_counts:
-            continue
-
-        # Extract file size and status code from the line
-        file_size = int(parts[5])
-        status_code = int(parts[4])
-
-        # Update total file size and status code counts
-        total_file_size += file_size
-        status_code_counts[status_code] = status_code_counts.get(status_code, 0) + 1
-
-        # Print metrics every 10 lines
-        if line_number % 10 == 0:
-            print_metrics()
-
-except KeyboardInterrupt:
-    # Print metrics on keyboard interruption (CTRL + C)
-    print_metrics()
-
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            split_string = re.split('- |"|"| " " ', str(line))
+            statusC_and_file_s = split_string[-1]
+            if counter != 0 and counter % 10 == 0:
+                printCodes(statusC_counter, file_size)
+            counter = counter + 1
+            try:
+                statusC = int(statusC_and_file_s.split()[0])
+                f_size = int(statusC_and_file_s.split()[1])
+                # print("Status Code {} size {}".format(statusC, f_size))
+                if statusC in statusC_counter:
+                    statusC_counter[statusC] += 1
+                file_size = file_size + f_size
+            except:
+                pass
+        printCodes(statusC_counter, file_size)
+    except KeyboardInterrupt:
+        printCodes(statusC_counter, file_size)
+        raise
